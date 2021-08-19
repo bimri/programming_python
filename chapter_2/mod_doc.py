3f9fb848b253eb15d424839b7baf51a8f31afa6a
bimri <bimri@outlook.com>
2021-08-19 19:36:19 +0300

    module documentation sources

---
+++ chapter_2/mod_doc.py
@@ -0,0 +1,9 @@
+"Module Documentation Sources"
+'''
+if you want to know everything that a built-in module exports, you can
+read its library manual entry; study its source code; or fetch its 
+attribute list and documentation string interactively.
+'''
+
+import sys
+print(dir(sys))
