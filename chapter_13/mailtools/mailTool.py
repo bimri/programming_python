2fc292dc0fe38dcce71057696351bdba8e59a71f
bimri <bimri@outlook.com>
2021-10-18 01:11:44 +0300

    the mailtools utility package

---
+++ chapter_13/mailtools/mailTool.py
@@ -0,0 +1,16 @@
+"The mailtools Utility Package"
+'MailTool Class'
+
+"""
+###############################################################################
+common superclasses: used to turn trace massages on/off
+###############################################################################
+"""
+
+class MailTool:                    # superclass for all mail tools
+    def trace(self, message):      # redef me to disable or log to file
+        print(message)
+
+class SilentMailTool:              # to mixin instead of subclassing
+    def trace(self, message):
+        pass
