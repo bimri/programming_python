a7aed1aeadc7306ad745e255780801456e533f91
bimri <bimri@outlook.com>
2021-10-20 02:55:47 +0300

    altconfigs: configuring for multiple accounts

---
+++ chapter_14/altconfigs/PyMailGui.py
@@ -0,0 +1,5 @@
+"altconfigs: Configuring for Multiple Accounts"
+
+import sys                             # ..\PyMailGui.py or 'book' for book configs
+sys.path.insert(1, '..')               # add visibility for real dir
+exec(open('../PyMailGui.py').read())   # do this, but get mailconfig here
