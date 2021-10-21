07f270302dd8666aee7573fce44b6aa028b81ec3
bimri <bimri@outlook.com>
2021-10-22 02:47:34 +0300

    Transferring Files to Clients and Servers

--- chapter_15/cgi-bin/languages-src.py
+++ chapter_15/cgi-bin/languages-src.py
@@ -10,3 +10,9 @@ print("<H1>Source code: '%s'</H1>" % filename)
 print('<HR><PRE>')
 print(cgi.escape(open(filename).read()))    # decode per platform default
 print('</PRE><HR>')
+
+"""
+Note that here, too, it’s crucial to format the text of the file with cgi.escape, because
+it is embedded in the HTML code of the reply. If we don’t, any characters in the text
+that mean something in HTML code are interpreted as HTML tags.
+"""
