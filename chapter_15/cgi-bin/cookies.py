fcf08d5a33bde2140eebc3bc05276b31fd46026f
bimri <bimri@outlook.com>
2021-10-22 00:55:08 +0300

    using cookies in CGI scripts

---
+++ chapter_15/cgi-bin/cookies.py
@@ -0,0 +1,26 @@
+"Using cookies in CGI scripts"
+'''
+lists a CGI script that stores a clientside
+cookie when first visited and receives and displays it on subsequent visits.
+'''
+
+"""
+create or use a client-side cookie storing username;
+there is no input form data to parse in this example
+"""
+
+import http.cookies, os
+cookstr = os.environ.get("HTTP_COOKIE")
+cookies = http.cookies.SimpleCookie(cookstr)
+usercook = cookies.get("user")                      # fetch if sent
+
+if usercook == None:
+    cookies = http.cookies.SimpleCookie()           # print Set-cookie hdr
+    cookies['user'] = 'Brian'
+    print(cookies)
+    greeting = '<p>His name shall be... %s</p>' % cookies['user']
+else:
+    greeting = '<p>Welcome back, %s</p>' % usercook.value
+
+print('Content-type: text/html\n')                  # plus blank line now
+print(greeting)                                     # and teh actual html
